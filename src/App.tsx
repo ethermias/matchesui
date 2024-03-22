c4652b1819b0c163352bef590b5252b60fa2fa33
Jermi Shiferaw <jermi.shiferaw@nielsen.com>
2024-03-22 19:46:12 -0400

    todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,30 @@
-// App.tsx
-import React from 'react';
-import styled from 'styled-components';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Card from './components/Card';
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+import TodoSummary from "./components/TodoSummary";
+import useTodos from "./hooks/useTodos";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-`;
+function App() {
+  const {
+    todos,
+    addTodo,
+    setTodoCompleted,
+    deleteTodo,
+    deleteAllCompletedTodos,
+  } = useTodos();
 
-const Content = styled.div`
-  flex: 1;
-  display: flex;
-`;
-
-const Side = styled.div`
-  flex: 1;
-  padding: 10px;
-`;
-const Middle = styled.div`
-  flex: 2;
-  padding: 10px;
-  width: 60vh;
-`;
-
-const App: React.FC = () => {
   return (
-    <Container>
-      <Header />
-      <Content>
-        <Side/>
-        <Middle>
-          <Card size="small" >
-            s
-          </Card>
-          <Card size="small" >
-            d
-          </Card>
-          <Card size="medium" >
-            d
-          </Card>
-          <Card size="large">
-            d
-          </Card>
-        </Middle>
-        <Side/>
-      </Content>
-      <Footer />
-    </Container>
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
+      <h1 className="font-bold text-3xl text-center">My Squad</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm onSubmit={addTodo} />
+        <TodoList
+          todos={todos}
+          onCompletedChange={setTodoCompleted}
+          onDelete={deleteTodo}
+        />
+      </div>
+      <TodoSummary todos={todos} deleteAllCompleted={deleteAllCompletedTodos} />
+    </main>
   );
 }
 

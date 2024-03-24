18ccc03effa9a03e72b173dd2969405bd2f34f21
Jermi Shiferaw <jermi.shiferaw@nielsen.com>
2024-03-24 10:45:56 -0400

    match30 version

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,16 +1,21 @@
+const sch = [
+  "Newcastle vs West Ham",
+  "Bournemouth vs Everton",
+  "Chelsea vs Burnley",
+  "Nott'm Forest vs Crystal Palace",
+  "Sheffield Utd vs Fulham",
+  "Spurs vs Luton",
+  "Aston Villa vs Wolves",
+  "Brentford vs Man Utd",
+  "Liverpool vs Brighton",
+  "Man City vs Arsenal"
+]
 export default function Schedule() {
   return (
     <div className="space-y-2">
-    <h2 className="text-sm font-bold mb-0">Schedule</h2>
+    <h2 className="text-sm font-bold mb-0">30 ሳምንት</h2>
     <div className="grid grid-cols-2">
-        <div>        
-            <div className="border p-0">Arseanl vs Man City</div>
-            <div className="border p-0">Watford vs Liverpool</div>
-            <div className="border p-0">Wesham vs Chealse </div>
-            <div className="border p-0">Chealse vs Brantford </div>
-            <div className="border p-0">Man United vs Man City </div>
-            <div className="border p-0">Aston Vila vs Wolved</div>
-        </div>
+        { sch.map((p) => <div className="border p-0" key={p}>{p}</div>) }
     </div>
 </div> 
   );

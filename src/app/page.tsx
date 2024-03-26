331ae5cc83658799a34e27456e3c4ea40741b78a
Jermi Shiferaw <jermi.shiferaw@nielsen.com>
2024-03-26 15:48:46 -0400

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,21 @@
 import usePlayers from "../hooks/usePlayers" 
 import SearchPlayers from "../components/player/searchPlayers"
 import ShowPlayers from "../components/player/ShowPlayers"
+import { useState } from "react";
+import { Player } from "../types/player";
 
 export default function Home() {
+  // const [players, setplayers] = useState<Array<Player>>([]);
   const {
     players,
     searchPlayers
   } = usePlayers() 
   return (<>
     <SearchPlayers onSubmit={searchPlayers}/>
-    <ShowPlayers />
-    <div>
+    <ShowPlayers players={players}/>
+    {/* <div>
         {players.length !== 0 && players.map((p) => <div key={p.id}> {p.name} </div>)}
-    </div>
+    </div> */}
   </>
   ) 
 }
--- src/components/Player/ShowPlayers.tsx
+++ src/components/Player/ShowPlayers.tsx
@@ -1,15 +1,44 @@
-'use client'
-import usePlayers from "@/src/hooks/usePlayers";
+"use client";
+import * as React from "react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Player } from "@/src/types/player";
 
-const ShowPlayers: React.FC = () => {
-    const {
-        players
-      } = usePlayers() 
-    return (
-      <div>
-        {players.length !== 0 && players.map((p) => <div key={p.id}> {p.name} </div>)}
-      </div>
-    );
-  };
-  
+interface ShowPlayersProps {
+  players: Player[];
+}
+const ShowPlayers: React.FC<ShowPlayersProps> = ({ players }) => {
+  console.log(players);
+  return (
+    <div style={{ maxWidth: "300px", paddingLeft: '40px' }}>
+      <Carousel
+          opts={{
+            align: "center",
+            loop: true,
+          }}
+      >
+        <CarouselContent>
+          {players.map((player, index) => (
+            <CarouselItem key={index}>
+                <Card >
+                    <CardHeader>
+                        <CardTitle>{player.jersey} {player.name}</CardTitle>
+                        <CardDescription>Arsenal $500</CardDescription>
+                    </CardHeader>
+                </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
 export default ShowPlayers;

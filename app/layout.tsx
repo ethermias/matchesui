0c5bcb694f4077c3e4defb17260ca7b047c075e6
Jermi Ethermias <ethermias@Jermis-MacBook-Pro.local>
2024-04-02 06:47:24 -0400

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,17 +21,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <div className="py-10 h-screen space-y-5 overflow-y-auto">
-        <h1 className="font-bold text-3xl text-center">ችቦ</h1> 
-          <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
-            <TagBar />
-            {children}
-            <Toaster />
+        <div className="flex flex-col h-screen">
+          <div className="fixed left-0 right-0 max-w-lg mx-auto bg-slate-50 py-4 px-6 z-10">
+            <h1 className="font-bold text-3xl text-center">ችቦ</h1>
+          </div>
+          <div className="py-10 flex-grow overflow-y-auto">
+            <div className="max-w-lg mx-auto bg-slate-10 rounded-md p-100 space-y-6">
+              <TagBar />
+              {children}
+              <Toaster />
+            </div>
+          </div>
+          <div className="fixed left-0 right-0 bottom-0 max-w-lg mx-auto bg-slate-50 py-4 px-6 z-10">
             <NavBar />
           </div>
-      </div>  
-        
-     </body>
+        </div>
+      </body>
     </html>
   );
 }

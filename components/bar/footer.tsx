f855acf93036e03591fccc1623b178294f516fb8
Jermi Ethermias <ethermias@Jermis-MacBook-Pro.local>
2024-04-03 18:10:56 -0400

    fixes

--- components/bar/footer.tsx
+++ components/bar/footer.tsx
@@ -1,10 +1,17 @@
+'use client'
 import Link from "next/link";
 import { Trophy, CalendarCheck2, Scale, LucideHome } from "lucide-react";
 import { Label } from "../ui/label";
+import { useTheme } from 'next-themes';
 
+const bgColor = (tm: string | undefined) => tm == 'light' ?  'bg-slate-100' : 'bg-gray-900'
+const txColor = (tm: string | undefined) => tm == 'light' ?  '' : 'bg-white' 
 export default function Footer() {
+  const { theme } = useTheme()
+
+  const tmName = `fixed left-0 right-0 bottom-0 max-w-lg mx-auto py-4 px-6 z-10 ${bgColor(theme)} ${txColor(theme)}`
   return ( 
-      <div className="fixed left-0 right-0 bottom-0 max-w-lg mx-auto bg-slate-100 py-4 px-6 z-10">
+      <div className={tmName}>
         <div className="text-center space-y-2">
           <div className="flex items-center space-x-4">
             <Link href="/">
--- components/bar/header.tsx
+++ components/bar/header.tsx
@@ -2,10 +2,16 @@
 import Setting from "./setting";
 import Dashboard from "./dashboard";
 import UserProfile from "./userProfile";
+import { useTheme } from 'next-themes';
 
+const bgColor = (tm: string | undefined) => tm == 'light' ?  'bg-slate-100' : 'bg-gray-900'
+const txColor = (tm: string | undefined) => tm == 'light' ?  '' : 'bg-white' 
 export default function Home() {
+    const { theme } = useTheme()
+    const tmName = `flex fixed left-0 right-0 max-w-lg mx-auto py-4 px-6 z-10 ${bgColor(theme)} ${txColor(theme)}`
+    console.log(tmName)
     return (
-        <div className="flex fixed left-0 right-0 max-w-lg mx-auto bg-slate-100 py-4 px-6 z-10">
+        <div className={tmName}>
             <Dashboard />
             <div className="flex-1 p-2">
                 <h1 className="font-bold text-3xl text-center">ችቦ</h1>

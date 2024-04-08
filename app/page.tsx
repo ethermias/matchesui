48612e43c3b8496e1c208ba5e7bf555e161a1ce3
Jermi Ethermias <ethermias@Jermis-MacBook-Pro.local>
2024-04-07 20:47:43 -0400

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import AddSquad from "../components/squad/addSquad"
 import Leaders from "@/components/leader/leaders"
 import { Label } from "@/components/ui/label"
 
-const deployMode = process.env.DEPLOY_MODE || 'S3';
+const deployMode = process.env.DEPLOY_MODE || 'Ec2';
 
 export default function Home() {
   function runningMode() {
--- components/bar/settingItems.tsx
+++ components/bar/settingItems.tsx
@@ -1,10 +1,10 @@
 
 "use client"
- 
+
 import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
- 
+
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
@@ -12,38 +12,44 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Label } from "../ui/label"
 
 function SettingItems({ className }: React.ComponentProps<"form">) {
-    const { setTheme } = useTheme()
-    return (
-        <div>  
-        <h2>👇👇👇 👇👇👇👇👇👇👇 👇👇👇  </h2>   
-        <div>👇 👇👇👇 👇👇👇 👇👇👇 </div> 
-        <h2>👇👇👇 👇👇👇 </h2>   
-        <div>👇👇👇  </div> 
-         <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => setTheme("light")}>
-            Light
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
-            Dark
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("system")}>
-            System
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+  const { setTheme } = useTheme()
+  return (
+    <div>
+      <div className="grid gap-2">
+        <h2>👇👇👇 👇👇👇👇👇👇👇 👇👇👇  </h2>
       </div>
- 
-    )
+      <div className="grid gap-2">
+        <Label>Change Theme</Label>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="grid gap-2">
+        <Label>Language</Label>
+      </div>
+    </div>
+
+  )
 }
 
 export default SettingItems

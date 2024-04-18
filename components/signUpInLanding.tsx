76ee589fd158c124fa3d72750dd99883d6d4c41b
Jermi Ethermias <ethermias@Jermis-MacBook-Pro.local>
2024-04-18 19:20:04 -0400

    fixes

--- components/signUpInLanding.tsx
+++ components/signUpInLanding.tsx
@@ -1,4 +1,3 @@
-import { ReactNode } from 'react';
 import React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
@@ -7,11 +6,11 @@ import BasicCard from './basic/BasicCard';
 export function SignUpInLanding() {
     return  <BasicCard title='🎈 Welcome back!' description='Enter your credentials and jump right back into the action.🎈'>
     <Link href="/signin">
-      <p> <a href="#" className="button">🔑 👉 👉 : Already part of the party 💃💃?&nbsp;&nbsp;<Button>Sign In</Button></a></p>
+      <p> 🔑 👉 👉 : Already part of the party 💃💃?&nbsp;&nbsp;<Button>Sign In</Button></p>
     </Link>
     <p>🎈Let&apos;s make memories, share laughs, and create unforgettable experiences together!🎈</p>
     <Link href="/signup">
-      <p> <a href="#">🚀 👉 👉 New to the party 💃💃? &nbsp;&nbsp;<Button>Sign Up</Button></a></p>
+      <p> 🚀 👉 👉 New to the party 💃💃? &nbsp;&nbsp;<Button>Sign Up</Button></p>
     </Link>
   </BasicCard>
 }
